6c01d18b1742b4838524a8fce854cc8a381f3245
Vaschell Mkenzie <mvaschell@gmail.com>
2023-08-15 17:29:44 -0400

    half finished

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,39 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    def __init__(self, name, breed, id=None):
+        self.id = id
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs
+                (id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT)
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS dogs
+        """
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.breed))
+        # self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, breed):
+        dog = cls(name, breed)
+        dog.save()
+
+        return dog
